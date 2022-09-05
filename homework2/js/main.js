691ea610c1d784b2d247a89125d4289506c9f2fd
Anastasia Fomina <anastasya.fomina.2001@gmail.com>
2022-09-05 16:14:02 +0300

    Homework 2

---
+++ homework2/js/main.js
@@ -0,0 +1,44 @@
+//There are steps a - d:
+let myName = 'Anastasia';
+console.log('Name:', myName);
+
+let mySurname = 'Fomina';
+console.log('Surname:', mySurname);
+
+let myCity = 'Odessa';
+console.log('City:', myCity);
+
+let mean = (10 + 5 + 2 + 12) / 4;
+console.log('Average:', mean);
+
+/* There are steps e - i:
+We calculate the area and length of a circle,
+display the wrong variables,
+and create comments in these steps.
+ */
+let radius = 5;
+console.log('Circle radius:', radius);
+
+let square = null;
+console.log('Square:', square);
+
+let length;
+console.log('Length:', length);
+
+const pi = 3.14;
+console.log('Pi:', pi);
+
+square = pi * radius ** 2;
+console.log('Circle square:', square);
+
+let d = 2 * radius;
+console.log('D:', d);
+length = pi * d;
+console.log('Circle length', length);
+
+/*
+Wrong variables in JS:
+let 12variable;
+let variab-le;
+let valeable?;
+ */
